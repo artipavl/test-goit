f555f05e144263bcee9ab07261ff679dae854356
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-04-28 14:17:04 +0300

    fix css

--- src/components/tweet/tweet.js
+++ src/components/tweet/tweet.js
@@ -4,18 +4,15 @@ import Button from 'components/button/button';
 
 import Goit from 'img/logo.svg';
 import Picture from 'img/picture.svg';
-// import Ellipse from 'img/Ellipse.png';
 
 const Tweet = ({ user }) => {
   return (
     <Box>
       <Logo src={Goit} alt="Logo SVG" width="76" height="22" />
       <Cover bg={Picture}></Cover>
-      {/* <img src={Picture} alt="Cover" width="308" height="168" /> */}
       <User>
         <Avatar>
           <img src={user.avatar} alt={user.user} width="64" height="64" />
-          {/* <EllipseEl src={Ellipse} alt="Ellipse" /> */}
         </Avatar>
         <Text>{user.tweets} tweets</Text>
         <Text>{user.followers} Followers</Text>
